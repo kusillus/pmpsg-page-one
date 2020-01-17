59e1fbc16b7b88bfe6bb410b31fe4a62f3596eb4
Wilder Alexander Guevara Benites <wguevarab7@gmail.com>
2020-01-17 12:03:30 -0500

    Primer commit de la demo

---
+++ page_one.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mf-wrapper-page">
+        hola soy la página 1
+        <span class="message">
+            {{saludo}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DemoExternalComponent',
+    data(){
+        return {
+            saludo: 'Hola amiguitos bienvenidos a mi componente.'
+        }
+    }
+
+}
+</script>
+
+<style lang="scss">
+.mf-wrapper-page{
+    background: orange;
+    width: 100%;
+    .message{
+        animation: changeColor 1s infinite alternate;
+    }
+}
+@keyframes changeColor {
+    to {
+        background-color: yellow;
+    }
+    from {
+        background-color: yellowgreen;
+    }
+}
+</style>
